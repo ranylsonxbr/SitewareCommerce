e93b931263b264b30d40b4582acf72684d6f080d
ranylsonxbr <ranylson1212@gmail.com>
2024-01-24 21:21:11 -0300

    implementa contrato da APi

--- Siteware/src/main/java/br/com/siteware/app/Cliente/application/api/ClienteAPI.java
+++ Siteware/src/main/java/br/com/siteware/app/Cliente/application/api/ClienteAPI.java
@@ -1,18 +1,21 @@
 package br.com.siteware.app.Cliente.application.api;
 
 
+
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 
+
+
 @RestController
 @RequestMapping("/v1/cliente")
 public interface ClienteAPI {
-    @PostMapping
-    @ResponseStatus(code = HttpStatus.CREATED)
-    ClienteResponse postCliente(@Valid @RequestBody ClienteRequest clienteRequest);
+
+    @PostMapping(value = "/cria-cliente")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    ClienteResponse postCliente(@RequestBody @Valid ClienteRequest ClienteRequest);
 
 }
-
 
